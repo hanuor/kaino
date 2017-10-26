ea8fe519bafdd38c37cadd12406e8b7281a21384
hanuor <hanuortech@gmail.com>
2017-10-26 07:43:35 +0530

    Location manager added

--- app/src/main/java/in/hanuor/kaino/MainActivity.kt
+++ app/src/main/java/in/hanuor/kaino/MainActivity.kt
@@ -1,12 +1,34 @@
 package `in`.hanuor.kaino
 
-import android.support.v7.app.AppCompatActivity
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var locationManager: LocationManager = this.getSystemService(android.content.Context.LOCATION_SERVICE) as LocationManager
+        var locationListener = object:LocationListener{
+            override fun onLocationChanged(p0: Location?) {
+                //get location
+            }
+
+            override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+                //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onProviderEnabled(p0: String?) {
+                //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onProviderDisabled(p0: String?) {
+                //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        }
     }
 }
