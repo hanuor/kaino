bc3b59cd7e126ca3ac83b77d9693dc36fafb2fce
hanuor <hanuortech@gmail.com>
2017-10-27 11:16:05 +0530

    Added gps location code

---
+++ app/src/main/java/in/hanuor/kaino/GPSLocationListener.kt
@@ -0,0 +1,38 @@
+package `in`.hanuor.kaino
+
+import android.location.Location
+import android.location.LocationListener
+import android.os.Bundle
+
+/*
+ * Copyright (C) 2016 Hanuor Inc. by Shantanu Johri(https://hanuor.github.io/shanjohri/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+class GPSLocationListener: LocationListener {
+    override fun onLocationChanged(p0: Location?) {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderEnabled(p0: String?) {
+       // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderDisabled(p0: String?) {
+     //   TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
--- app/src/main/java/in/hanuor/kaino/MainActivity.kt
+++ app/src/main/java/in/hanuor/kaino/MainActivity.kt
@@ -1,6 +1,7 @@
 package `in`.hanuor.kaino
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
@@ -8,17 +9,20 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 
+
+
 class MainActivity : AppCompatActivity() {
 
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("Hey, "," sadasd")
         var locationManager: LocationManager = this.getSystemService(android.content.Context.LOCATION_SERVICE) as LocationManager
         var locationListener = object : LocationListener {
             override fun onLocationChanged(p0: Location?) {
                 //get location
-                Log.d("Hey location!", "" + p0?.accuracy + p0?.latitude + p0?.longitude)
+                Log.d("Hey location!", " bbbbbbb " + p0?.accuracy +" ???  " +  p0?.latitude + p0?.longitude)
             }
 
             override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
@@ -34,6 +38,12 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0f, locationListener)
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+
+        val GPSlocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val gpsLocationListener = GPSLocationListener()
+        GPSlocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000, 10f, gpsLocationListener)
+
+
     }
 }
